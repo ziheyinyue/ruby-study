23ca00935c49c043dee85c7a11be37669e8a3066
ziheyinyue <hakuka1992@gmail.com>
2020-02-10 06:27:41 +0000

    bug fix

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    if @artile.save
+    if @article.save
       flash[:notice] = "Article was successfully created"
       redirect_to article_path(@article)
     else
