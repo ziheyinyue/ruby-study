d694f5407de85e5c1d8d58e23a93a3d7e43fdeea
ziheyinyue <hakuka1992@gmail.com>
2020-02-11 06:46:45 +0000

    create users

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200210024457) do
+ActiveRecord::Schema.define(version: 20200211064024) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
   end
 
 end
