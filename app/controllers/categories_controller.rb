293f2548bd56861c98730e2bf3e34d57c11970f7
ziheyinyue <hakuka1992@gmail.com>
2020-02-14 02:25:23 +0000

    edit categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -19,7 +19,20 @@ class CategoriesController < ApplicationController
   end
   
   def show
-    
+    @category=Category.find(params[:id])
+    @category_articles=@category.articles.paginate(page: params[:page], per_page:10)
+  end
+  
+  def edit
+    @category=Category.find(params[:id])
+  end
+  
+  def update
+    @category=Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success]="Category name was successfully updated"
+      redirect_to category_path(@category)
+    end
   end
   
   private
